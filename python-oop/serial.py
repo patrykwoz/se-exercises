817b4d5675a238c505c1a40397ec6364a8ae0e8f
root <root@PATRYK-NFHTHTE0>
2023-10-04 14:35:34 -0400

    Finished Python OOP Exercise

--- python-oop/serial.py
+++ python-oop/serial.py
@@ -19,4 +19,28 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=0):
+        """
+        Initialize the SerialGenerator with a starting value.
 
+        Args:
+            start (int, optional): The starting value for the serial numbers. Default is 0.
+        """
+        self.start = start
+        self.counter = start - 1
+    def __repr__(self):
+        return f"<SerialGenerator(start={self.start})>"
+
+    def generate(self):
+        """
+        Generate the next serial number.
+
+        Returns:
+            int: The next serial number.
+        """
+        self.counter += 1
+        return self.counter
+
+    def reset(self):
+        """Reset the serial number generator to the initial starting value."""
+        self.counter = self.start
--- python-oop/wordfinder.py
+++ python-oop/wordfinder.py
@@ -1,5 +1,52 @@
 """Word Finder: finds random words from a dictionary."""
+from random import choice
 
 
 class WordFinder:
-    ...
+    """Machine read a .txt file and pick random words from that file
+
+    >>>wf = WordFinder("test.txt")
+    1 words read
+
+    >>>wf.random()
+    'test'
+    """
+    def __init__(self, path):
+        self.path = path
+        self.list_words = self.read_words()
+        self.init_msg()
+    def __repr__(self):
+        return f"<WordFinder('{self.path}')>"
+        
+    def read_words(self):
+        words_from_file = []
+        with open(self.path, 'r') as file:
+            for line in file:
+                words_from_file.append(line.strip())
+        return words_from_file
+    def random(self):
+        rnd_word = choice(self.list_words)
+        return rnd_word
+    def init_msg(self):
+        msg = f'{len(self.list_words)} words read.'
+        print(msg)
+
+
+class SpecialWordFinder(WordFinder):
+    """Machine read a .txt file and pick random words from that file
+    it will ignore empty lines and comments starting with #
+
+    >>>sf = SpecialWordFinder("test_special.txt")
+    3 words read
+
+    >>>sf.random()
+    'test'
+    """
+    def __init__(self,path):
+        super().__init__(path)
+        self.list_words = self.remove_special()
+    def remove_special(self):
+        clean_words = [word for word in self.list_words if ((word != '') and (word[0] != '#'))]
+        return clean_words
+    def __repr__(self):
+        return f"<SpecialWordFinder('{self.path}')>"
